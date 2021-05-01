8d2300ee2eb25f23139f09772fe96d98d1fd8178
runny-yolk <theo.stevens03@gmail.com>
2021-05-01 21:23:30 +0100

    Update Team.tsx
    Updated the Team component to be in line with recent DB/API changes, and to support the new layout. See comments for more detail.

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { getRoles, Role } from "../utils/Roles";
+import {RoleView} from "./RoleView";
 
 export class TeamData {
   author: string;
@@ -9,10 +10,12 @@ export class TeamData {
   id: number;
   constructor(teamJSON: Record<string, unknown>){
     this.author = teamJSON.author as string;
-    // using lorem as the description, awaiting DB change:
+    // the DB does spit out descriptions now, but they're kinda short
     // this.description = teamJSON.description as string;
     this.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim"
-    this.createdAt = new Date(teamJSON.createdAt as string);
+    // currently broken :/
+    // this.createdAt = new Date(teamJSON.createdAt as string);
+    this.createdAt = new Date();
     // currently using a random role instead of the one from the DB, to introduce some variation
     // this.skills = getRoles(teamJSON.skillsetMask as number);
     this.skills = getRoles(1 + (Math.random() * 1023));
@@ -25,14 +28,18 @@ export const Team: React.FC<{team:TeamData}> = ({team}) => {
   var h = (Date.now() - team.createdAt.valueOf()) / 3600000;
   var timestr = h.toFixed(1) + " Hours ago"
 
-  var skillstr = team.skills.map(r => r.name).toString().replaceAll(',', ', ');
+  var skillstr = team.skills.map(r => <RoleView className="w-7 fill-bright inline-block m-1 align-top" r={r} key={r.id}/>);
 
   return (
-    <div className="py-3">
-      <div>{team.author}</div>
-      <div>{team.description}</div>
-      <div>{timestr}</div>
-      <div>{skillstr}</div>
+    <div className="my-10 p-5 border relative">
+      <div className="absolute -top-2.5 left-1 px-3 bg-black leading-none font-bold text-lg">{team.author}</div>
+      <div className="flex justify-between">
+        <div className="mr-5 text-lg">{team.description}</div>
+        <div>
+          <div className="mb-1">🕓 {timestr}</div>
+          <div className="text-lg w-36">👀 {skillstr}</div>
+        </div>
+      </div>
     </div>
   )
 }
