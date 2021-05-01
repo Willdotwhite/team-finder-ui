34cb4e56a2bf6a1d443edba23fa7b7a6d7731ed8
Dallon Feldner <dallon.feldner@gmail.com>
2021-05-01 16:11:34 -0500

    Added a register page

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { PageContainer } from "../../components/PageContainer";
+import { PageHeader } from "../../components/PageHeader";
+
+export const Register: React.FC = () => {
+  return (
+    <PageContainer>
+      <PageHeader>Register a Team</PageHeader>
+    </PageContainer>
+  );
+};
