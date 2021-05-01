bd46c55db1ca794ef5606b31ae7a337bae0d63f3
runny-yolk <theo.stevens03@gmail.com>
2021-05-01 08:45:24 +0100

    Refactoring Teams into Data class + other stuff
    Put all the team-related stuff into src/components/Team, and created a data class for processing the JSON into something richer and more useful
    
    The TeamData class ignores the name field, and uses Lorem Ipsum as placeholder for the description field, awaiting DB/API change
    
    the Team component displays info in a way that's somewhat more useful and a lot less horrible
    
    The homepage is now less placeholder - the button now refetches team data instead of incrementing a counter, and the JSX is a bit shallower

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,50 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import { useQuery } from "react-query";
-import { getRoles } from "../../utils/Roles";
-
-interface Team {
-  author: string,
-  createdAt: string,
-  name: string,
-  skillsetMask: number,
-  id: number
-}
-
-const Teams: React.FC = () => {
-  const { isLoading, isError, data } = useQuery(["someQuery"], async (): Promise<Array<Team>> => {
-    return fetch("http://178.62.53.195/teams", {mode:"cors"}).then(res => res.json());
-  });
-
-  return <div>{
-    isLoading ? "Loading.." : isError ? "fuck" : data!.map(t => <div key={t.id} className="p-3">
-        <div>{t.name}</div>
-        <div>{t.author}</div>
-        <div>{t.createdAt}</div>
-
-        {/*<div>{getRoles(t.skillsetMask)}</div>*/}
-        {<div>{JSON.stringify(getRoles(1 + (Math.random() * 1023)))}</div>}
-        <div>{t.id}</div>
-      </div>)
-  }</div>
-}
+import { TeamData, Team } from "../../components/Team"
 
 export const Home: React.FC = () => {
-  const [count, setCount] = useState(0);
+	
+  const { isLoading, isError, data, refetch } = useQuery(["Teams"], async (): Promise<Array<TeamData>> => {
+    var arr: Array<Record<string, unknown>> = await fetch("http://178.62.53.195/teams", {mode:"cors"}).then(res => res.json());
+    return arr.map(t => new TeamData(t));
+  });
 
   return (
-    <div className="container mx-auto my-6">
-      <h1 className="text-3xl text-primary font-light px-6 pb-6">
-        Hello World
+    <div className="container mx-auto my-6 p-2">
+      <h1 className="text-3xl text-primary font-light my-6">
+        Find a team!
       </h1>
-      <div className="border border-white p-6 space-y-3 border-opacity-25">
-        <button
-          className="bg-primary-dark p-2 focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-primary"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is: {count}
-        </button>
-        <Teams/>
-      </div>
+
+      <button
+        className="bg-primary-dark my-6 p-2 focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-primary"
+        onClick={() => refetch()}
+      >
+        Refetch Teams
+      </button>
+
+      <div>{
+        isLoading ? "Loading.." : isError ? "fuck" : data!.map(t => <Team key={t.id} team={t}/>)
+      }</div>
     </div>
   );
 };
