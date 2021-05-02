55a97741f2a66b792a0fa46855e42f6b6050704e
Dallon Feldner <dallon.feldner@gmail.com>
2021-05-01 19:21:17 -0500

    Validation with react-hook-form

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,30 +1,65 @@
 import * as React from "react";
+import { NestedValue, useForm } from "react-hook-form";
+import classNames from "classnames";
+import immer from "immer";
 import { Button } from "../../components/Button";
 import { PageContainer } from "../../components/PageContainer";
 import { PageHeader } from "../../components/PageHeader";
 import { roles } from "../../utils/Roles";
 
+interface FormData {
+  description: string;
+  skillsets: NestedValue<Record<number, boolean>>;
+}
+
 export const Register: React.FC = () => {
-  const [description, setDescription] = React.useState("");
-  const [skillsets, setSkillsets] = React.useState(new Set<number>());
+  const {
+    register,
+    formState,
+    handleSubmit,
+    setValue,
+    watch,
+  } = useForm<FormData>({
+    defaultValues: {
+      description: "",
+      skillsets: {},
+    },
+  });
+
+  React.useEffect(() => {
+    register("skillsets", {
+      validate: (value) => {
+        if (Object.keys(value).length === 0) {
+          return "Required";
+        }
+      },
+    });
+  }, [register]);
+
+  const skillsets = watch("skillsets");
 
   const toggleSkillset = (id: number) =>
-    setSkillsets((current) => {
-      const next = new Set(current.values());
-      if (next.has(id)) {
-        next.delete(id);
-      } else {
-        next.add(id);
+    setValue(
+      "skillsets",
+      immer(skillsets, (prev) => {
+        if (prev[id]) {
+          delete prev[id];
+        } else {
+          prev[id] = true;
+        }
+      }),
+      {
+        shouldValidate: true,
       }
-      return next;
-    });
+    );
 
   return (
     <PageContainer>
       <PageHeader>Register a Team</PageHeader>
       <form
         className="max-w-prose mx-auto space-y-4"
-        onSubmit={(e) => e.preventDefault()}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        onSubmit={handleSubmit(() => {})}
       >
         <div className="space-y-2">
           <label className="text-lg block">
@@ -40,24 +75,38 @@ export const Register: React.FC = () => {
                   type="checkbox"
                   className="mr-1"
                   onChange={() => toggleSkillset(role.id)}
-                  checked={skillsets.has(role.id)}
+                  checked={Boolean(skillsets[role.id])}
                 />
                 {role.name}
               </label>
             ))}
           </div>
+          {formState.errors.skillsets && (
+            <div className="text-red-400">
+              {formState.errors.skillsets?.message}
+            </div>
+          )}
         </div>
         <div className="space-y-2">
           <label className="text-lg block" htmlFor="description">
             Tell us a bit about yourself and the team you&rsquo;d like to see!
           </label>
           <textarea
-            className="text-md bg-transparent border border-white focus:border-primary px-4 py-2 block w-full placeholder-white placeholder-opacity-40 h-40"
+            className={classNames(
+              "text-md bg-transparent border  px-4 py-2 block w-full placeholder-white placeholder-opacity-40 h-40",
+              formState.errors.description
+                ? "border-red-400 focus:border-red-500"
+                : "border-white focus:border-primary"
+            )}
             placeholder="Hi, I'm Mark! I've been learning Unity for about a year, but this is my first jam. I like level design, and I can do a bit of programming too! I like platformers and games that make it feel good to move around. I'd love to pair up with an artist and maybe a programmer."
             id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            {...register("description", { required: "Required" })}
           />
+          {formState.errors.description && (
+            <div className="text-red-400">
+              {formState.errors.description.message}
+            </div>
+          )}
           <div className="prose prose-sm text-white text-opacity-70">
             <p>It&rsquo;s good to talk about:</p>
             <ul>
