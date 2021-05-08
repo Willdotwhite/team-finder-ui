18705bdee5c57e883dd80e28a218562acba43869
Brysen <9168041+awildbrysen@users.noreply.github.com>
2021-05-08 20:13:23 +0200

    Authentication interaction with backend
    This is mainly example code on how the interaction with the backend for authentication would happen
    
    On `/login` these is a sign in with discord button, this starts the auth flow
    If this flow is done and
    Succeeds: the backend will redirect to `/login/authorized` with the created jwt token in the query string
    Failes: the backend will redirect to `/login/failed` with the reason of failure in the query string
    
    The AuthReq and /req route is testing a fetch to a authenticated endpoint using the token

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Register } from "./pages/Register/Register";
+import { Login } from "./pages/Login/Login"
+import { AuthorizedCallback } from "./pages/Login/AuthorizedCallback";
+import { AuthReq } from "./pages/Login/AuthReq";
 
 export const Routes: React.FC = () => (
   <Switch>
@@ -10,6 +13,17 @@ export const Routes: React.FC = () => (
     </Route>
     <Route exact={true} path="/register">
       <Register />
+    </Route>
+    <Route exact={true} path="/login">
+      <Login />
+    </Route>
+    <Route exact={true} path="/login/failed">
+    </Route>
+    <Route exact={true} path="/login/authorized">
+      <AuthorizedCallback />
+    </Route>
+    <Route exact={true} path="/req">
+      <AuthReq />
     </Route>
   </Switch>
 );
