f7966072d19d0a21952c1dbb2ea6f7ce9ae43adf
WillDotWhite <willdotwhiteis@gmail.com>
2021-05-12 12:20:53 +0100

    Enforce common standard for path naming

--- src/components/PageNavigator.tsx
+++ src/components/PageNavigator.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom"
 export const PageNavigator: React.FC = () => (
   <div className="flex flex-row justify-center items-center my-14">
     <NavLink to="/" exact={true} activeClassName="text-primary font-medium hover:no-underline" className="text-3xl font-light text-white text-center mx-16 uppercase whitespace-pre hover:underline">Find Team</NavLink>
-    <NavLink to="/Register" activeClassName="text-primary font-medium hover:no-underline" className="text-3xl font-light text-white text-center mx-16 uppercase whitespace-pre hover:underline">Register / Edit{"\n"}Team</NavLink>
+    <NavLink to="/register" activeClassName="text-primary font-medium hover:no-underline" className="text-3xl font-light text-white text-center mx-16 uppercase whitespace-pre hover:underline">Register / Edit{"\n"}Team</NavLink>
   </div>
 );
 
