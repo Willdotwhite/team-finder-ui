b4a9c112e5320fd2a7ee9ec4c9c58c289c02d76c
Dallon Feldner <dallon.feldner@gmail.com>
2021-05-15 16:52:35 -0500

    Removed unused import

--- src/components/PageNavigator.tsx
+++ src/components/PageNavigator.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const PageNavigator: React.FC = () => (
   <div className="flex flex-row justify-center items-center my-8">
