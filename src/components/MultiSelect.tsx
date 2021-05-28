efa27041f17e5fb85c4b6cd42cd6473ffd4a5206
runny-yolk <theo.stevens03@gmail.com>
2021-05-28 18:55:00 +0100

    Bulk of improvements
    MultiSelect now looks way nicer, and has nicer APIs! And doesn't need users to be told how to use it as it's more consistent with other multi selects.

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,49 +1,65 @@
 import * as React from "react";
 
+const Selected: React.FC<{onClick: () => void}> = ({onClick, children}) => (
+  <div
+    className="flex mb-3 mr-5 leading-none cursor-pointer border rounded border-gray-400 bg-gray-700"
+    onClick={onClick}
+  >
+    <div className="p-1.5">x</div>
+    <div className="border-gray-400 border-l p-1.5 pb-2">{children}</div>
+  </div>
+)
+
+const Selectables: React.FC<{
+  valueDisplayIndex: Record<string, string>;
+  selectableValues: string[];
+  toggleValue: (s: string) => void;
+}> = ({selectableValues, valueDisplayIndex, toggleValue}) => {
+  return (
+    <select
+      value=""
+      onChange={e => toggleValue(e.target.value)}
+      className="p-1 pb-1.5 outline-none w-full rounded bg-gray-700"
+    >
+      <option value="">Click here to select a language..</option>
+      {selectableValues.map((p, i) =>
+        (<option value={p} key={i}>{valueDisplayIndex[p]}</option>)
+      )}
+    </select>
+  );
+}
+
 type Props = {
   disabled: boolean;
-  possibleVals: string[];
-  displayVals: string[];
-  values: string[];
-  changeCallback?: (values: string[]) => unknown;
+  valueDisplayIndex: Record<string, string>;
+  selected: string[];
+  changeCallback: (selected: string[]) => unknown;
 }
-export const MultiSelect: React.FC<Props> = (props) => {
-  const [values, setValues] = React.useState(props.values);
-  React.useEffect(() => setValues(props.values), [props.values])
+export const MultiSelect: React.FC<Props> = ({valueDisplayIndex, selected, ...props}) => {
 
-  const displayIndex: Record<string, string> = {};
-  props.possibleVals.forEach((x, i) => displayIndex[x] = props.displayVals[i]);
-
-  const ToggleValue = (value: string) => {
+  const toggleValue = (value: string) => {
     if(props.disabled) return;
 
     let newvalues: string[];
 
-    if(values.includes(value)) newvalues = values.filter(v => v != value);
-    else newvalues = [...values, value];
+    if(selected.includes(value)) newvalues = selected.filter(v => v != value);
+    else newvalues = [...selected, value];
 
-    setValues(newvalues);
-    if(props.changeCallback) props.changeCallback(newvalues);
+    props.changeCallback(newvalues);
   }
 
+  const selectableValues = Object.keys(valueDisplayIndex).filter(p => !selected.includes(p));
 
-  return (<div>
-    {values.map((v, i) => (<div
-      className="inline-block mr-5 leading-none cursor-pointer border-b border-transparent hover:border-white"
-      key={i}
-      onClick={() => ToggleValue(v)}
-    >
-      {displayIndex[v]}
-    </div>))}
-    <select
-      value=""
-      onChange={e => ToggleValue(e.target.value)}
-      className="text-black block p-1 pb-1.5 mt-1 outline-none"
-    >
-      <option value=""></option>
-      {props.possibleVals.filter(p => !values.includes(p)).map((p, i) =>
-        (<option value={p} key={i}>{displayIndex[p]}</option>)
-      )}
-    </select>
-  </div>)
+  return (
+    <div className="flex flex-wrap rounded border-white">
+      <div className="flex flex-wrap flex-shrink-0 max-w-full">
+        {selected.map((v, i) => (
+          <Selected key={i} onClick={() => toggleValue(v)}>{valueDisplayIndex[v]}</Selected>
+        ))}
+      </div>
+      <div className="flex-grow mb-3">
+        <Selectables {...{valueDisplayIndex, selectableValues, toggleValue}} />
+      </div>
+    </div>
+  )
 }
