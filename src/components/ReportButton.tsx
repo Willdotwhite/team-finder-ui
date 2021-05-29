99d37cf46f322863d309234f52515df10a8d66df
runny-yolk <theo.stevens03@gmail.com>
2021-05-29 19:08:51 +0100

    Ensures timezone parsed as UTC for Teams + better mobile display

--- src/components/ReportButton.tsx
+++ src/components/ReportButton.tsx
@@ -38,7 +38,7 @@ export const ReportButton: React.FC<{teamId:string, className?:string}> = ({team
     forceUpdate();
   };
 
-  return (<div onClick={handleClick} className="inline-block cursor-pointer text-black hover:text-red-500 transition-colors">
+  return (<div onClick={handleClick} className={baseClass+" inline-block cursor-pointer text-black hover:text-red-500 transition-colors pr-4"}>
     <ReactSVG
       src="/Flag.svg"
       className={
