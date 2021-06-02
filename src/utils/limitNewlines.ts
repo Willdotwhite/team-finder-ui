263792ec7c74c988c1299c3a883ee3c5163012d2
Dallon Feldner <dallon.feldner@gmail.com>
2021-06-02 08:00:24 -0500

    Actually use that constant

--- src/utils/limitNewlines.ts
+++ src/utils/limitNewlines.ts
@@ -1,8 +1,10 @@
+import { NUM_NEWLINES } from "./consts";
+
 const newlineRegex = /\r?\n/g;
 export const limitNewlines = (input: string): string => {
   const newlines = [...input.matchAll(/\r?\n/g)];
-  if (newlines.length > 10) {
-    const firstNewlineToBeRemoved = newlines[10];
+  if (newlines.length > NUM_NEWLINES) {
+    const firstNewlineToBeRemoved = newlines[NUM_NEWLINES];
     return (
       input.slice(0, firstNewlineToBeRemoved.index) +
       input.slice(firstNewlineToBeRemoved.index).replace(newlineRegex, "")
